cd4f506048611c27752c2da416358d959e68b7d9
David Murillo <david.josue.murillo23@gmail.com>
2024-07-26 20:29:52 -0500

    Consumiendo API para la obtención de imagenes

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,7 +1,23 @@
+const getGifs = async (category) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=Eu7yg0Ri3FcQ7TdRSbcLh44DEuQbeUOS&q=${category}&limit=20`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }));
+
+    console.log(gifs);
+    return gifs;
+}
+
 
 export const GifGrid = ({ category }) => {
 
-
+    getGifs(category);
 
     return (
         <>
