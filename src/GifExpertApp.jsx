5587f964e741ee86b3f91c325c2a3cc3f459286c
David Murillo <david.josue.murillo23@gmail.com>
2024-07-26 19:14:46 -0500

    Repasando el uso de useState

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react"
+import { AddCAtegory } from "./components/AddCAtegory";
 
 export const GifExpertApp = () => {
-  return (
-    <>
-        <h1>GifExpertApp</h1> 
-    </>
-  )
+
+    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+    return (
+        <>
+            <h1>GifExpertApp</h1>
+
+            <AddCAtegory
+                categories={categories}
+                setCategories={setCategories}
+            />
+
+            <ol>
+                {
+                    categories.map(category => (
+                        <li key={category}>{category}</li>
+                    ))
+                }
+            </ol>
+        </>
+    )
 }
---
+++ src/components/AddCAtegory.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+export const AddCAtegory = ({categories,setCategories}) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    // Funcion para actualizar el estado del input
+    const onInputChange = ({target}) => {
+        setInputValue(target.value);
+    }
+
+    // Funcion para agregar la categoria al estado
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if(inputValue.trim().length <= 1) return; // Si el input esta vacio no se agrega la categoria
+        
+        setCategories([...categories, inputValue]); // Se agrega la categoria al estado
+        setInputValue(''); // Se vacia el input cada vez que se agrega una categoria
+    }
+
+    return (
+        <form>
+            <input 
+                type="text" 
+                placeholder="Buscar gifs"
+                value={ inputValue }
+                onChange={ onInputChange } 
+                />
+
+            <button type="submit" onClick={ onSubmit }>Agregar</button>
+        </form>
+    )
+}
+
+ 
